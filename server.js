b86e93b112bb01e9faf866ecdd0c2a6713e4d61e
Michael De Jesus <michaeldejesus2@Michaels-MacBook-Pro-2.local>
2018-09-13 17:19:09 -0300

    rest commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const hbs = require('hbs');
+
+let app = express();
+
+app.set('view engine', 'hbs');
+
+
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs');
+//     next();
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('userActive' , () => {
+    return 'Michael De Jesus';
+});
+
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        titlePage: 'Home'
+    }); 
+});
+
+app.get('/about', (req, res) =>{
+    res.render('about.hbs', {
+        titlePage: 'About Page'
+    });
+});
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('server is up in port 3000!!!');
+});
